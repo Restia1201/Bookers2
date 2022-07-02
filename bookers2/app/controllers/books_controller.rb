73b6dae873ed98d14a77f8f628303bbe56dd64e4
Restia1201 <yoshiki1201.1995@gmail.com>
2022-07-02 01:36:14 +0000

    課題：アプリケーションを作成してみよう：応用編

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,68 @@
+class BooksController < ApplicationController
+  before_action :move_to_signed_in
+
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+     flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book.id)
+    else
+     @user = current_user
+     @books = Book.all
+     render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def show
+    @book = Book.new
+    @find_book = Book.find(params[:id])
+    @user = @find_book.user
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def edit
+   @book = Book.find(params[:id])
+   if @book.user == current_user
+     render :edit
+   else
+     redirect_to books_path
+   end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully"
+     redirect_to book_path(@book)
+    else
+     render :edit
+    end
+  end
+
+
+  private
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+    end
+  end
+
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+end
---
+++ bookers2/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+
+  get '/home/about' => 'homes#about', as:'about'
+
+  devise_scope :user do
+   delete '/users/sign_out' => 'devise/sessions#destroy'
+  end
+end
